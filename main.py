79753d51cf08d2d612fe0828954006a687720c98
CalvoM <mwadimemakokha@ieee.org>
2020-06-20 17:01:33 +0300

    Accessible as an endpoint

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 from Unfurler import Unfurler
+from flask import Flask, request, Response, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return {"msg": "Karibu Nyumbani"}
+
+@app.route("/unfurl/", methods= ["POST"])
+def unfurl():
+    data = request.get_json()
+    f = Unfurler.Unfurler(data.get('url'))
+    data =f.unfurl()
+    return jsonify(data)
 
 if __name__ == "__main__":
-    f = Unfurler.Unfurler("https://www.youtube.com/watch?v=ZRiTwiP-1_k")
-    print(f.unfurl())
+    app.run(debug=True)
