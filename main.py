4bbd4da4b608a021b1eceea4c115f4ecdda5caa1
CalvoM <mwadimemakokha@ieee.org>
2020-06-20 14:12:33 +0300

    Add documentation and improve function order

--- main.py
+++ main.py
@@ -3,22 +3,35 @@ from bs4 import BeautifulSoup
 from typing import List
 import requests
 
-twitter_tags = []
-og_tags =[]
-def get_twitter_tags(meta_tags : List ):
-    #Twitter uses meta["name"]
+
+def get_twitter_tags(meta_tags: List, twitter_tags: List):
+    """
+    Extracts the twitter meta Tags from the list of meta tags
+    Adds the extracted twitter meta tags to the list of twitter tags *twitter_tags*
+
+    Args:
+        param1 meta_tags (List) - List containing meta tags
+        param2 twitter_tags (List) - List to hold twitter meta tags
+    """
     for meta in meta_tags:
-        meta_name:str = meta.get("name")
+        meta_name: str = meta.get("name")
         if meta_name and meta_name.startswith("twitter:"):
-            split_tag : List = meta_name.split(":")
-            if len(split_tag) > 2:
+            split_tag: List = meta_name.split(":")
+            if len(split_tag) > 2:# some tags have more than one colon separator e.g image:src
                 twit_tag_ = split_tag[1] + ":" + split_tag[2]
             else:
                 twit_tag_ = split_tag[1] 
             twitter_tags.append({twit_tag_:meta.get("content")})
 
-def get_og_tags(meta_tags : List):
-    #Opengraph uses meta["property"]
+def get_og_tags(meta_tags : List, og_tags : List):
+    """
+    Extracts the Open graph meta Tags from the list of meta tags
+    Adds the extracted Open graph meta tags to the list of open_graph tags *og_tags*
+
+    Args:
+        param1 meta_tags (List) - List containing meta tags 
+        param2 og_tags (List) - List to hold og meta tags
+    """
     for meta in meta_tags:
         meta_name : str = meta.get("property")
         if meta_name and (meta_name.startswith("og:") or meta_name.startswith("fb:")):
@@ -47,6 +60,7 @@ except FileNotFoundError:
 
 soup = BeautifulSoup(resp_text,"lxml")
 metas = soup.find_all('meta')
-get_og_tags(metas)
+og_tags =[]
+get_og_tags(metas, og_tags)
 for t_tag in og_tags:
     print(t_tag)
