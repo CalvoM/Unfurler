35408b9ca13a600cfab52d506dbcc0f8d6f696c7
CalvoM <mwadimemakokha@ieee.org>
2020-07-18 17:00:27 +0300

    Reconfigure entry to heroku app

--- main.py
+++ main.py
@@ -1,21 +1,4 @@
-from Unfurler import Unfurler
-from flask import Flask, request, Response, jsonify, abort
-
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return {"msg": "Welcome to the unfurling site"}
-
-@app.route("/unfurl/", methods= ["POST"])
-def unfurl():
-    data = request.get_json()
-    url: str = data.get('url')
-    if url is None:
-        return abort(400,"Missing essentials")
-    f = Unfurler.Unfurler(url)
-    data =f.unfurl()
-    return jsonify(data)
+from Unfurler import app
 
 if __name__ == "__main__":
     app.run()
