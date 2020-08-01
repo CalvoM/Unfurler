ee279a2e7fa59042c0d278cedb8dc534677cd15b
CalvoM <mwadimemakokha@ieee.org>
2020-08-01 21:52:15 +0300

    reduce debug statements

--- src/cache.py
+++ src/cache.py
@@ -3,11 +3,8 @@ from src import redis_
 def check_url(url):
     is_url = redis_.get(url)
     if is_url is None: 
-        print(url,"Not Found")
         raise KeyError
-    print(url,"Found")
     return is_url
 
 def add_url(url,data):
-    print(url,"Added")
-    redis_.set(url,data)
+    redis_.set(url,dat1a)
