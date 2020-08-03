4239bff61880351323e7103956ec7b8ea2443b2e
CalvoM <mwadimemakokha@ieee.org>
2020-08-03 14:28:52 +0300

    Cache responses from scraping

--- go.mod
+++ go.mod
@@ -5,6 +5,6 @@ go 1.14
 require (
 	github.com/CalvoM/Unfurler v0.0.2
 	github.com/andybalholm/cascadia v1.2.0 // indirect
-	github.com/gomodule/redigo v1.8.2
+	github.com/go-redis/redis/v8 v8.0.0-beta.7
 	golang.org/x/net v0.0.0-20200707034311-ab3426394381 // indirect
 )
--- main.go
+++ main.go
@@ -1,14 +1,22 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
-	"github.com/CalvoM/Unfurler"
-	"github.com/gomodule/redigo/redis"
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/CalvoM/Unfurler"
+	"github.com/go-redis/redis/v8"
 )
+
+type Response struct {
+	data map[string]map[string]string
+}
+
+var ctx = context.Background()
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	str := `<html>
@@ -34,18 +42,28 @@ func unfurlHandler(w http.ResponseWriter, r *http.Request) {
 		if err != nil {
 			log.Fatal(err)
 		}
-		c,err:=redis.DialURL(os.Getenv("REDIS_URL"))
-		if err!=nil{
+		c, err := redis.ParseURL(os.Getenv("REDIS_URL"))
+		if err != nil {
 			log.Fatal(err)
 		}
-		defer c.Close()
-		r,err:=c.Do("GET",u.Url)
-		fmt.Println(r)
-		uf := Unfurler.Unfurler{Url: u.Url}
-		data := uf.Unfurl()
-		jsonVal, _ := json.Marshal(data)
-		w.Header().Set("Content-Type", "application/json")
-		w.Write(jsonVal)
+		c.Username = "" // Solve ERR wrong number of arguments of 'auth' error
+		rdb := redis.NewClient(c)
+		val, err := rdb.Get(ctx, u.Url).Result()
+		if err == redis.Nil {
+			uf := Unfurler.Unfurler{Url: u.Url}
+			data := uf.Unfurl()
+			jsonVal, _ := json.Marshal(data)
+			_, err := rdb.Set(ctx, u.Url, jsonVal, 0).Result()
+			if err != nil {
+				log.Fatal(err)
+			}
+			w.Header().Set("Content-Type", "application/json")
+			w.Write(jsonVal)
+		} else {
+			w.Header().Set("Content-Type", "application/json")
+			w.Write([]byte(val))
+		}
+
 	default:
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		fmt.Fprintln(w, "Method is not allowed")
@@ -54,16 +72,16 @@ func unfurlHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	port:=os.Getenv("PORT")
-	fmt.Println("Running on",port)
-	if port==""{
+	port := os.Getenv("PORT")
+	fmt.Println("Running on", port)
+	if port == "" {
 		log.Fatal("PORT is not set")
 	}
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", homeHandler)
 	mux.HandleFunc("/unfurl/", unfurlHandler)
 	server := &http.Server{
-		Addr:":"+port,
+		Addr:    ":" + port,
 		Handler: mux,
 	}
 	_ = server.ListenAndServe()
